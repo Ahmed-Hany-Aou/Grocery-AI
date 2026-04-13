48843d8774403c1b53035e3509608a58fc4c8606
Ahmed-Hany-Aou <125566073+Ahmed-Hany-Aou@users.noreply.github.com>
2026-04-13 07:09:13 +0200

    Seed fruits and vegetables with example products
    Added seeding for fruits and vegetables categories along with products.

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -24,6 +24,33 @@ class DatabaseSeeder extends Seeder
                                     ]
                                 );
 
+                    $fruits = Category::updateOrCreate(['slug' => 'fruits'], ['name' => 'Fruits', 'icon' => 'fruit-icon']);
+                    $vegetables = Category::updateOrCreate(['slug' => 'vegetables'], ['name' => 'Vegetables', 'icon' => 'veg-icon']);
+
+                    Product::updateOrCreate(['slug' => 'apple'], [
+                                                        'name' => 'Red Apple',
+                                                        'price' => 1.99,
+                                                        'description' => 'Fresh red apple',
+                                                        'stock_quantity' => 100,
+                                                        'category_id' => $fruits->id,
+                                                        'barcode' => '123456789',
+                                                        'sku' => 'FRT-APL-001'
+                                                    ]);
+
+                    Product::updateOrCreate(['slug' => 'carrot'], [
+                                                        'name' => 'Organic Carrot',
+                                                        'price' => 0.99,
+                                                        'description' => 'Fresh organic carrot',
+                                                        'stock_quantity' => 200,
+                                                        'category_id' => $vegetables->id,
+                                                        'barcode' => '987654321',
+                                                        'sku' => 'VEG-CRT-001'
+                                                    ]);
+        }
+    }
+
+                                );
+
                 $fruits = Category::updateOrCreate(['slug' => 'fruits'], ['name' => 'Fruits', 'icon' => 'fruit-icon']);
                     $vegetables = Category::updateOrCreate(['slug' => 'vegetables'], ['name' => 'Vegetables', 'icon' => 'veg-icon']);
 
